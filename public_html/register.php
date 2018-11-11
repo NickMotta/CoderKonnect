cf761ce71190f11d745104bec81afba11d1231ef
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 05:24:49 -0500

    Update public_html/register.php

--- public_html/register.php
+++ public_html/register.php
@@ -21,15 +21,24 @@
 	
 	if($passhash1 != $passhash2)
 	{
-		die("ERROR! Retype your password!");
-	}
-	
-	$query = dbquery("SELECT d_email FROM Developer WHERE d_email=\"$email\";");
-	
-	if($query.fetch_object()->num_rows <= 0)
+        die("ERROR! Retype your password!");
+    }
+    else if($query.fetch_object()->num_rows <= 0)
 	{
+		$query = dbquery("SELECT d_email FROM Developer WHERE d_email=\"$email\";");
 		die("ERROR! Account already exists with that email!");
 	}
+	else
+	{
+        $query = dbquery("INSERT INTO Developer SET d_firstName='$firstName', d_lastName='$lastName,d_email='$email", d_password='$password');
+	}
+
+	if (!isset($query))
+	{
+		die ("ERROR! Something went wrong while trying to register a new user");
+	}
+
+
 	
 	
 	
