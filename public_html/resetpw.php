89c59e8b8b497b4311e3dcd67c6842f496ad8585
jerrys123111 <jerrys123111@gmail.com>
2018-11-11 12:02:11 -0500

    fix 3

--- public_html/resetpw.php
+++ public_html/resetpw.php
@@ -4,7 +4,7 @@
 	echo "<!DOCTYPE html>";
     echo "<HTML><BODY>";
 	
-	$realEmail = trim($_POST(["email"]);
+	$realEmail = trim($_POST["email"]);
 	$emailAddress = dbescape($realEmail);
 	$password = dbescape(trim($_POST["newpass"]));
 	
