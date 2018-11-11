7e195d323881892d1ab47877c978dc9eac2b2150
jerrys123111 <jerrys123111@gmail.com>
2018-11-11 04:11:47 -0500

    Big change finally

---
+++ public_html/resetpw.php
@@ -0,0 +1,18 @@
+<?php
+	require_once '../database.php';
+	
+	echo "<!DOCTYPE html>";
+    echo "<HTML><BODY>";
+	
+	$emailAddress = dbescape(trim($_POST["email"]));
+	$password = dbescape(trim($_POST["newpass"]));
+	
+	$passhash = passhash($password);
+	
+	$url = "http://142.93.204.188/CoderKonnect/public_html/verifypw.php?email=$emailAddress&password=$passhash";
+	
+	mail($emailAddress, "Verification Link", $url);
+	
+	echo "Please check email $emailAddress for verification link!";
+	echo "</BODY></HTML>";
+?>
---
+++ public_html/verifiypw.php
@@ -0,0 +1,14 @@
+<?php
+	require_once '../database.php';
+	
+	echo "<!DOCTYPE html>";
+    echo "<HTML><BODY>";
+	
+	$emailAddress = dbescape(trim($_GET["email"]));
+	$passhash = dbescape(trim($_GET["password"]));
+	
+	dbquery("UPDATE Developer SET d_passhash=\"$passhash\" WHERE d_email=\"$emailAddress\";");
+	
+	echo "Password has been successfully updated!";
+	echo "</BODY></HTML>";
+?>
