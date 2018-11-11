dcd6f2c5b545dac15429bc213cb488d9baa97a7b
jerrys123111 <jerrys123111@gmail.com>
2018-11-11 11:56:00 -0500

    Attempt 2

--- public_html/resetpw.php
+++ public_html/resetpw.php
@@ -4,14 +4,15 @@
 	echo "<!DOCTYPE html>";
     echo "<HTML><BODY>";
 	
-	$emailAddress = dbescape(trim($_POST["email"]));
+	$realEmail = trim($_POST(["email"]);
+	$emailAddress = dbescape($realEmail);
 	$password = dbescape(trim($_POST["newpass"]));
 	
 	$passhash = passhash($password);
 	
 	$url = "coderkonnect.com/verifypw.php?email=$emailAddress&password=$passhash";
 	
-	mail($emailAddress, "Verification Link", $url);
+	mail($realEmail, "Verification Link", $url);
 	
 	echo "Please check email $emailAddress for verification link!";
 	echo "</BODY></HTML>";
