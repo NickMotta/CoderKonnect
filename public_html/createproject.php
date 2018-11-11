5c7dcdc8962513de6b88f441b521dce3cc53f937
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 09:01:22 -0500

    yee

---
+++ public_html/createproject.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 11/11/2018
+ * Time: 8:49 AM
+ * Project: Lab_12
+ */
+
+
+
+
+
+?>
---
+++ public_html/updateProfile.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 11/11/2018
+ * Time: 8:50 AM
+ * Project: Lab_12
+ */
+
+//connect to database.
+require_once 'database.php';
+
+print_r($_POST[category]);
+
+
+
+
+/**
+ * This function will generate a string representation of different integers
+ * separated by a comma. It will then return that string.
+ */
+function generateStringRepresentation($array)
+{
+
+}
