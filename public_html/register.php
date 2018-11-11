24c52d54d05ee6280deddc3a3ab97ba8e77b8b43
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 08:38:29 -0500

    added confirmation page

--- public_html/register.php
+++ public_html/register.php
@@ -37,6 +37,50 @@ $confpass = "test123";
 	{
 		die ("ERROR! Something went wrong while trying to register a new user.");
 	}
+	else
+	{
+		echo <<<_END
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Register - Coder Konnect</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+    <nav class="navbar navbar-expand-sm bg-primary navbar-dark text-white rounded-0">
+        <a class="navbar-brand" href="index.html">Logo</a>
+        <ul class="nav nav-pills">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="login.html">Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link text-white" href="#">Register</a>
+            </li>
+        </ul>
+    </nav>
+
+</head>
+<body>
+<div class="container shadow-lg mb-4 bg-white rounded border border-primary mt-3">
+    <h1 class="display-1 text-center">Registration Successful!</h1>
+    <br><br>
+    <h1 class="display-5 text-center mb-5">Click the button below to be brought to the log-in screen.</h1>
+
+    <div class="container w-50">
+        <h1 class="display-5 text-center mt-5" style="margin-bottom: 0px">Or,</h1>
+        <form action="CreateProject.html" class="">
+            <button class="btn-primary btn-block rounded btn-lg mb-3"><h1 class="display-5">Create A Project</h1></button>
+        </form>
+    </div>
+</div>
+</body>
+
+
+
+_END;
+
+	}
 
 
 	
