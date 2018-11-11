e7f28b174109f6c00bc389c03067e6882132fb1d
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 10:13:41 -0500

    Update public_html/login.php

--- public_html/login.php
+++ public_html/login.php
@@ -29,8 +29,8 @@
 
 	//If there isn't an entry with that password
 	//Purposefully make the error message vague
-    echo "Results of res2: " . $res2->fetch_object() . "<br><br>";
-	if ($res2->fetch_object() !== $passwordHashed)
+    echo "Results of res2: " . $res2->fetch_object()->d_passhash . "<br><br>";
+	if ($res2->fetch_object()->d_passhash !== $passwordHashed)
 	{
 		die ("Wrong e-mail/password!");
 	}
