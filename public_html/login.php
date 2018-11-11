90725dd1057116233b37a99e56ca82bfff081b28
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 10:42:58 -0500

    Update public_html/login.php

--- public_html/login.php
+++ public_html/login.php
@@ -31,10 +31,11 @@
 	echo "res2: " . $res2 . "<br><br>";
 	echo "pwd: " . $pwd . "<br><br>";
 	echo "passtest: " . passtest($pwd, $res2) . "<br><br>";
+	echo "reverse passtest: " . !passtest($pwd, $res2) . "<br><br>";
 
 	//If there isn't an entry with that password
 	//Purposefully make the error message vague
-	if (!(passtest($pwd, $res2)))
+	if (!passtest($pwd, $res2))
 	{
 		die ("Wrong e-mail/password!");
 	}
