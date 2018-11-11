c8d070926adf93cc72639a47c92a3ba54261aad2
Nicholas Motta <nickmotta38@gmail.com>
2018-11-11 10:41:28 -0500

    Update public_html/login.php
    getting so sick of this

--- public_html/login.php
+++ public_html/login.php
@@ -20,11 +20,17 @@
 	{
 		die("entry not found");     //error entry not found
 	}
-	
+
+
+
+
 	//Gets the current hashed password from the database
 	$res2 = dbquery("SELECT * FROM Developer WHERE d_email='$email'");
 	$res2 = $res2->fetch_object()->d_passhash;
 
+	echo "res2: " . $res2 . "<br><br>";
+	echo "pwd: " . $pwd . "<br><br>";
+	echo "passtest: " . passtest($pwd, $res2) . "<br><br>";
 
 	//If there isn't an entry with that password
 	//Purposefully make the error message vague
